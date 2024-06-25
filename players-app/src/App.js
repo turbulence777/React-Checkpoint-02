ae98e8e19bab3b5c137e5cd4ea22ac126b4e4db6
turbulence777 <joshuaakonyiro72@gmail.com>
2024-06-25 09:47:35 +0100

    first commit

---
+++ players-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import PlayersList from './PlayersList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Players List</h1>
+      <PlayersList />
+    </div>
+  );
+}
+
+export default App;
